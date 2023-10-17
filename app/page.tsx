d98e7875f196df38a44b37eaf531c9a0dacee236
Micah Mills <micahmills@gmail.com>
2023-10-17 12:24:32 +0300

    fixes build error

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function AgentsPage() {
   );
   return (
     <div>
-      <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl text-center">Digital Responder Agents</h1>
+      <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl text-center">Digital Responder Agents</h1>
       <Navbar></Navbar>
     </div>
   );
