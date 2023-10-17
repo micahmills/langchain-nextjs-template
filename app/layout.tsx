0f86609c2452c9ca038c02515de396c27563c8fc
Micah Mills <micahmills@gmail.com>
2023-10-17 15:37:53 +0300

    Changed the titles on the share cards

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,14 +20,14 @@ export default function RootLayout({
           name="description"
           content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
         />
-        <meta property="og:title" content="LangChain + Next.js Template" />
+        <meta property="og:title" content="Digital Engagement Responder Bot" />
         <meta
           property="og:description"
           content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
         />
         <meta property="og:image" content="/images/og-image.png" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="LangChain + Next.js Template" />
+        <meta name="twitter:title" content="Digital Engagement Responder Bot" />
         <meta
           name="twitter:description"
           content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
